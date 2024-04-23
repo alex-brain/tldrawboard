c2f20784937403ebe3838657b1dfb4c20e70d570
Aleksandr <aleks-webby@yandex.ru>
2024-04-23 12:41:58 +0300

    Update dependencies

--- src/App.js
+++ src/App.js
@@ -1,124 +1,224 @@
-import { useState } from 'react'
-import { Box, Tldraw, TldrawImage } from 'tldraw'
-import 'tldraw/tldraw.css'
-import initialSnapshot from './snapshot.json'
+import "./App.css";
+import { Tldraw, createTLStore, defaultShapeUtils, throttle, TldrawImage } from "tldraw";
+import "./index.css";
+import "tldraw/tldraw.css";
+import { useEffect, useLayoutEffect, useState } from "react";
 
-export default function App() {
-  const [editor, setEditor] = useState()
-  const [snapshot, setSnapshot] = useState(initialSnapshot)
-  const [currentPageId, setCurrentPageId] = useState()
-  const [showBackground, setShowBackground] = useState(true)
-  const [isDarkMode, setIsDarkMode] = useState(false)
-  const [viewportPageBounds, setViewportPageBounds] = useState(new Box(0, 0, 600, 400))
-  const [isEditing, setIsEditing] = useState(false)
-  const [format, setFormat] = useState('svg')
+function App() {
+  function uuidv4() {
+    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+      /[xy]/g,
+      function (c) {
+        var r = (Math.random() * 16) | 0,
+          v = c == "x" ? r : (r & 0x3) | 0x8;
+        return v.toString(16);
+      }
+    );
+  }
+
+  const [boards, setBoards] = useState([]);
+
+  const localStorageArray = JSON.parse(localStorage.getItem("boards"));
+
+  if (!localStorageArray) {
+    localStorage.setItem("boards", JSON.stringify([]));
+  }
+
+  useEffect(() => {
+		const TLBoardsArray = localStorageArray?.map((item) => ({
+			id: item.id,
+			store: createTLStore({ shapeUtils: defaultShapeUtils }),
+			loadingState: { status: "loading" },
+		}));
+
+    setBoards([...boards, ...TLBoardsArray]);
+  }, []);
+
+  const addNewBoard = () => {
+    const newBoard = {
+      id: "board-" + uuidv4(),
+      store: createTLStore({ shapeUtils: defaultShapeUtils }),
+      loadingState: { status: "loading" },
+    };
+    setBoards([...boards, newBoard]);
+
+    const newBoardId = {
+      id: newBoard.id,
+    };
+    localStorageArray.push(newBoardId);
+    const updatedArray = JSON.stringify(localStorageArray);
+    localStorage.setItem("boards", updatedArray);
+  };
+
+  const [activeBoard, setActiveBoard] = useState(null);
+
+  const [store] = useState(() =>
+    createTLStore({ shapeUtils: defaultShapeUtils })
+  );
+  const [loadingState, setLoadingState] = useState({
+    status: "loading",
+  });
+  useLayoutEffect(() => {
+    setLoadingState({ status: "loading" });
+
+    // Get persisted data from local storage
+    boards?.forEach((board) => {
+      const persistedSnapshot = localStorage.getItem(board.id);
+
+      if (persistedSnapshot) {
+        try {
+          const snapshot = JSON.parse(persistedSnapshot);
+          console.log(snapshot);
+          board.store.loadSnapshot(snapshot);
+          board.snapshot = snapshot; 
+          setLoadingState({ status: "ready" });
+        } catch (error) {
+          setLoadingState({ status: "error", error: error.message }); // Something went wrong
+        }
+      } else {
+        setLoadingState({ status: "ready" }); // Nothing persisted, continue with the empty store
+      }
+
+      // Each time the store changes, run the (debounced) persist function
+      console.log(board.store);
+      // const snapshot = board.store.getSnapshot();
+      // console.log('snapshot', snapshot)
+      const cleanupFn = board.store.listen(
+        throttle(() => {
+          const snapshot = board.store.getSnapshot();
+          localStorage.setItem(board.id, JSON.stringify(snapshot));
+        }, 500)
+      );
+
+      return () => {
+        cleanupFn();
+      };
+    });
+    setLoadingState({ status: "ready" }); // Nothing persisted, continue with the empty store
+
+  }, [boards, activeBoard]);
   
-  console.log('isEditing', isEditing)
+  console.log('boards', boards)
+
+  if (loadingState.status === "loading") {
+    return (
+      <div className="tldraw__editor">
+        <h2>Loading...</h2>
+      </div>
+    );
+  }
+
+  if (loadingState.status === "error") {
+    return (
+      <div className="tldraw__editor">
+        <h2>Error!</h2>
+        <p>{loadingState.error}</p>
+      </div>
+    );
+  }
 
   return (
-    <div style={{ padding: 30 }}>
-      <div style={{ position: 'fixed', inset: 0 }}>
-        {isEditing ? (
-          <Tldraw
-            snapshot={snapshot}
-            onMount={(editor) => {
-              setEditor(editor)
-              editor.updateInstanceState({ isDebugMode: false })
-              editor.user.updateUserPreferences({ isDarkMode })
-              if (currentPageId) {
-                editor.setCurrentPage(currentPageId)
-              }
-              if (viewportPageBounds) {
-                editor.zoomToBounds(viewportPageBounds, { inset: 0 })
-              }
+    <div className="App">
+      {!activeBoard ? (
+        <>
+          <button onClick={addNewBoard}>Add new board</button>
+          <div
+            className="tldraw__container"
+            style={{
+              position: "fixed",
+              zIndex: -1,
+              inset: "30px 0 0",
+              overflowY: "auto",
             }}
-          />
-        ) : (
-          <div key={1} className="board-list">
-            <div className="board-item" onClick={() => {
-              if (isEditing) {
-                if (!editor) return
-                setIsDarkMode(editor.user.getIsDarkMode())
-                setShowBackground(editor.getInstanceState().exportBackground)
-                setViewportPageBounds(editor.getViewportPageBounds())
-                setCurrentPageId(editor.getCurrentPageId())
-                setSnapshot(editor.store.getSnapshot())
-                setIsEditing(false)
-              } else {
-                setIsEditing(true)
-              }
-            }}>
-              <TldrawImage
-                //[1]
-                snapshot={snapshot}
-                // [2]
-                pageId={currentPageId}
-                // [3]
-                background={showBackground}
-                darkMode={isDarkMode}
-                bounds={viewportPageBounds}
-                padding={0}
-                 scale={1}
-                format={format}
-              />
-            </div>
-            <div key={2} className="board-item" onClick={() => {
-              if (isEditing) {
-                if (!editor) return
-                setIsDarkMode(editor.user.getIsDarkMode())
-                setShowBackground(editor.getInstanceState().exportBackground)
-                setViewportPageBounds(editor.getViewportPageBounds())
-                setCurrentPageId(editor.getCurrentPageId())
-                setSnapshot(editor.store.getSnapshot())
-                setIsEditing(false)
-              } else {
-                setIsEditing(true)
-              }
-            }}>
-              <TldrawImage
-                //[1]
-                snapshot={snapshot}
-                // [2]
-                pageId={currentPageId}
-                // [3]
-                background={showBackground}
-                darkMode={isDarkMode}
-                bounds={viewportPageBounds}
-                padding={0}
-                scale={1}
-                format={format}
-              />
-            </div>
-            <div key={3} className="board-item" onClick={() => {
-              if (isEditing) {
-                if (!editor) return
-                setIsDarkMode(editor.user.getIsDarkMode())
-                setShowBackground(editor.getInstanceState().exportBackground)
-                setViewportPageBounds(editor.getViewportPageBounds())
-                setCurrentPageId(editor.getCurrentPageId())
-                setSnapshot(editor.store.getSnapshot())
-                setIsEditing(false)
-              } else {
-                setIsEditing(true)
-              }
-            }}>
-              <TldrawImage
-                //[1]
-                snapshot={snapshot}
-                // [2]
-                pageId={currentPageId}
-                // [3]
-                background={showBackground}
-                darkMode={isDarkMode}
-                bounds={viewportPageBounds}
-                padding={0}
-                scale={1}
-                format={format}
-              />
-            </div>
+          >
+            {boards?.map((board) => (
+              <div key={board.id} className="tldraw__item">
+                <div
+                  className="tldraw__item_cover"
+                  onClick={() => {
+                    setActiveBoard(board);
+                  }}
+                />
+                <TldrawImage
+                  store={board.store}
+                  snapshot={board.snapshot}
+                  className="tldraw__board disabled"
+                  hideUi
+                />
+              </div>
+            ))}
           </div>
-        )}
+        </>
+      ) : (
+        <div
+          style={{
+            position: "fixed",
+            inset: 0,
+            overflowY: "scroll",
+          }}
+        >
+          <p style={{ cursor: "pointer" }} onClick={() => setActiveBoard(null)}>
+            {`<<`} Назад
+          </p>
+          <Tldraw
+            store={activeBoard.store}
+            style={{ position: "absolute", zIndex: -1 }}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
+
+
+/*
+
+import { createContext, useContext, useState } from 'react'
+import { Editor, Tldraw } from 'tldraw'
+import 'tldraw/tldraw.css'
+
+const FocusedEditorContext = createContext({})
+
+export default function InlineExample() {
+  const [focusedEditor, setFocusedEditor] = useState(null)
+  return (
+    <FocusedEditorContext.Provider value={{ focusedEditor, setFocusedEditor }}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          padding: 32,
+          paddingTop: 12,
+          gap: 12,
+        }}
+      >
+        <InlineEditor width={500} height={300} />
+      </div>
+    </FocusedEditorContext.Provider>
+  )
+}
+
+function InlineEditor({ width, height }) {
+  const { focusedEditor, setFocusedEditor } = useContext(FocusedEditorContext)
+
+  const title = `${width} x ${height}`
+
+  const handleMount = (editor) => {
+    editor.updateInstanceState({ isDebugMode: false })
+    const shapshot = editor.store.getSnapshot();
+    console.log('shapshot', shapshot)
+  }
+
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div style={{ width, height }} onFocus={() => setFocusedEditor(title)}>
+        <Tldraw onMount={handleMount} autoFocus={focusedEditor === title} />
       </div>
     </div>
   )
 }
+ */
